383f737d3f213c53da015147f788bdaa63766f83
fluzzi <fnluzzi@gmail.com>
2024-06-23 21:25:26 -0300

    now files handler calls only request method from files service

--- internal/handlers/files_handler.go
+++ internal/handlers/files_handler.go
@@ -3,16 +3,12 @@ package handlers
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"file-editor/api"
+	"file-editor/pkg/files"
 )
 
 type FilesService interface {
-	ReadAllFiles(ctx context.Context, request *api.Empty) (*api.ReadAllFilesResponse, error)
-	ReadFile(filename string) (*api.ReadFileResponse, error)
-	SaveFile(ctx context.Context, request *api.SaveFileRequest) (*api.SaveFileResponse, error)
-	FindText(ctx context.Context, request *api.FindTextRequest) (*api.FindTextResponse, error)
-	DoRequest(command string, filename string) (response interface{}, err error)
+	Request(operationType files.OperationType, request interface{}) (response interface{}, err error)
 }
 
 type Handler struct {
@@ -27,31 +23,55 @@ func New(filesService FilesService) *Handler {
 }
 
 func (h *Handler) SaveFile(ctx context.Context, req *api.SaveFileRequest) (*api.SaveFileResponse, error) {
-	return h.FilesService.SaveFile(ctx, req)
-}
-
-func (h *Handler) FindText(ctx context.Context, req *api.FindTextRequest) (*api.FindTextResponse, error) {
-	return h.FilesService.FindText(ctx, req)
-}
-
-func (h *Handler) ReadAllFiles(ctx context.Context, req *api.Empty) (*api.ReadAllFilesResponse, error) {
-	return h.FilesService.ReadAllFiles(ctx, req)
-}
-
-func (h *Handler) ReadFile(ctx context.Context, req *api.ReadFileRequest) (*api.ReadFileResponse, error) {
-	var res *api.ReadFileResponse
-	response, err := h.FilesService.DoRequest("READ", req.Filename)
+	response, err := h.FilesService.Request(files.Save, req)
 	if err != nil {
 		return nil, err
 	}
 
-	b, ok := response.(string)
-	if !ok {
-		return nil, errors.New("test")
+	res := &api.SaveFileResponse{}
+	if err := json.Unmarshal([]byte(response.(string)), res); err != nil {
+		return nil, err
 	}
 
-	err = json.Unmarshal([]byte(b), res)
+	return res, nil
+}
+
+func (h *Handler) FindText(ctx context.Context, req *api.FindTextRequest) (*api.FindTextResponse, error) {
+	response, err := h.FilesService.Request(files.Find, req)
 	if err != nil {
+		return nil, err
+	}
+
+	res := &api.FindTextResponse{}
+	if err := json.Unmarshal([]byte(response.(string)), res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (h *Handler) ReadAllFiles(ctx context.Context, req *api.Empty) (*api.ReadAllFilesResponse, error) {
+	response, err := h.FilesService.Request(files.ReadAll, req)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &api.ReadAllFilesResponse{}
+	if err := json.Unmarshal([]byte(response.(string)), res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (h *Handler) ReadFile(ctx context.Context, req *api.ReadFileRequest) (*api.ReadFileResponse, error) {
+	response, err := h.FilesService.Request(files.Read, req)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &api.ReadFileResponse{}
+	if err := json.Unmarshal([]byte(response.(string)), res); err != nil {
 		return nil, err
 	}
 
